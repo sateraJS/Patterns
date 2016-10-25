0a79ae135dd386e199bd2cd88a14b52342fadea8
satera <j7smith1732@gmail.com>
2016-10-25 22:37:38 +0500

    CarFactory

---
+++ HW2/HW2/CarFactory.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    public interface ICarFactory
+    {
+        IBody CreateBody();
+        IEngine CreatEngine();
+        IVehicle CreateVehicle();
+    }
+    public interface IBody
+    {
+        string Name { get; }
+    }
+
+    public interface IEngine
+    {
+        string Name { get; }
+    }
+
+    public interface IVehicle
+    {
+        string Name { get; }
+    }
+
+    #region BMW
+    public class BMWFactory: ICarFactory
+    {
+        public IBody CreateBody()
+        {
+            return new BMWBody();
+        }
+
+        public IEngine CreatEngine()
+        {
+            return new BMWEngine();
+        }
+
+        public IVehicle CreateVehicle()
+        {
+            return new BMWVehicle();
+        }
+    }
+    public class BMWBody : IBody
+    {
+        public string Name
+        { 
+            get{ return "Кузов BMW";}
+        }
+    }
+
+    public class BMWEngine : IEngine
+    {
+        public string Name
+        { 
+            get{ return "Двигатель BMW";}
+        } 
+    }
+
+    public class BMWVehicle : IVehicle
+    {
+        public string Name
+        { 
+            get{ return "Салон BMW";}
+        }
+    }
+    #endregion
+
+    #region AUDI
+    public class AUDIFactory : ICarFactory
+    {
+        public IBody CreateBody()
+        {
+            return new AUDIBody();
+        }
+
+        public IEngine CreatEngine()
+        {
+            return new AUDIEngine();
+        }
+
+        public IVehicle CreateVehicle()
+        {
+            return new AUDIVehicle();
+        }
+    }
+    public class AUDIBody : IBody
+    {
+        public string Name
+        { 
+            get{ return "Кузов AUDI";}
+        }
+    }
+
+    public class AUDIEngine : IEngine
+    {
+        public string Name 
+        { 
+            get{ return "Двигатель AUDI";}
+        }
+    }
+
+    public class AUDIVehicle : IVehicle
+    {
+        public string Name
+        {
+            get { return "Салон AUDI"; }
+        }
+    }
+    #endregion
+}
---
+++ HW2/HW2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var bmwFactory = new BMWFactory();
+            PrintMenu(bmwFactory, "BMW");
+
+            var audiFactory = new AUDIFactory();
+            PrintMenu(audiFactory, "AUDI");
+
+             Console.ReadKey();
+        }
+
+        public static void PrintMenu(ICarFactory factory, string name)
+        {
+            Console.WriteLine(string.Format("****** Фабрика {0} ******", name));
+            var bode = factory.CreateBody();
+            Console.WriteLine(bode.Name + '\n');
+
+            var engine = factory.CreatEngine();
+            Console.WriteLine(engine.Name + '\n');
+
+            var vehicle = factory.CreateVehicle();
+            Console.WriteLine(vehicle.Name + '\n');
+        }
+    }
+}
